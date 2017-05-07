805893d0c8798020a4f79dd6e4f28b0f7679a131
scalexm <martin.alex32@hotmail.fr>
2017-05-07 18:48:21 +0200

    Add tests for L2 norm

--- img.py
+++ img.py
@@ -1,87 +1,145 @@
 import numpy as np
 import math
-from PIL import Image
+import cv2
+from os import *
+from os.path import *
 
-def extract_pixels(im, treshold = 0):
-    (w, h) = im.size
-    pixels = []
+def extract_pixels(img):
+    _, img = cv2.threshold(
+        img,
+        0,
+        255,
+        cv2.THRESH_BINARY + cv2.THRESH_OTSU
+    )
 
-    for x in range(w):
-        for y in range(h):
-            if im.getpixel((x, y)) > treshold:
-                pixels.append([x, y])
+    h, w = img.shape[:2]
+    pixels = [[x, y] for y in range(h) for x in range(w) if img[y, x] > 0]
     return np.array(pixels).T
 
-def pca(pixels):
-    (values, vectors) = np.linalg.eig(np.cov(pixels))
+def get_pc(pixels):
+    values, vectors = np.linalg.eig(np.cov(pixels))
+    i = 0 if values[0] > values[1] else 1
 
-    if values[1] > values[0]:
-        values[0], values[1] = values[1], values[0]
-        vectors = vectors[::-1,:]
+    angle = math.atan2(vectors[i][1], vectors[i][0]) * 180 / math.pi
+    scales = [math.sqrt(values[i]), math.sqrt(values[1 - i])]
 
-    angle = math.atan2(vectors[0][1], vectors[0][0]) * 180 / math.pi
-    scales = [int(math.sqrt(values[0])), int(math.sqrt(values[1]))]
-
+    if angle < 0:
+        angle += 180
     return (angle, scales)
 
 def get_center(pixels):
-    x = np.sum(pixels[0, : ]) / pixels.shape[1]
-    y = np.sum(pixels[1, : ]) / pixels.shape[1]
-
+    x = np.sum(pixels[0, :]) / pixels.shape[1]
+    y = np.sum(pixels[1, :]) / pixels.shape[1]
     return (x, y)
 
-def change_basis(im):
-    (w, h) = im.size
-    pixels = extract_pixels(im)
-    (angle, scales) = pca(pixels)
-    (center_x, center_y) = get_center(pixels)
+def change_basis(img, center, angle):
+    h, w = img.shape[:2]
+    c_x, c_y = center
 
-    new_im = im.rotate(
-        (-1) * angle,
-        expand = True,
-        center = (center_x, center_y),
-        translate = (w / 2 - center_x, h / 2 - center_y)
-    )
+    M = cv2.getRotationMatrix2D((c_x, c_y), angle, 1.0)
+    cos = np.abs(M[0, 0])
+    sin = np.abs(M[0, 1])
+    w = int((h * sin) + (w * cos))
+    h = int((h * cos) + (w * sin))
+    M[0, 2] += (w / 2) - c_x
+    M[1, 2] += (h / 2) - c_y
 
-    return (new_im, scales)
+    return cv2.warpAffine(img, M, (w, h))
 
-def mutual_rescale(imm1, scale1, imm2, scale2):
-    scale_x = max(scale1[0], scale2[0])
-    scale_y = max(scale1[1], scale2[1])
+def normalize(img):
+    pixels = extract_pixels(img)
+    angle, scales = get_pc(pixels)
+    center = get_center(pixels)
+    return (change_basis(img, center, -angle), scales)
 
-    (w1, h1) = imm1.size
-    (w2, h2) = imm2.size
+def L2(img1, img2):
+    h1, w1 = img1.shape[:2]
+    h2, w2 = img2.shape[:2]
+    h, w = min(h1, h2), min(w1, w2)
 
-    w1 *= scale_x / scale1[0]
-    h1 *= scale_y / scale1[1]
-    w2 *= scale_x / scale2[0]
-    h2 *= scale_y / scale2[1]
+    r1 = img1[(h1 - h) // 2 : (h1 + h) // 2, (w1 - w) // 2 : (w1 + w) // 2]
+    r2 = img2[(h2 - h) // 2 : (h2 + h) // 2, (w2 - w) // 2 : (w2 + w) // 2]
+    return cv2.norm(r1, r2)
 
-    im1 = imm1.resize((int(w1), int(h1)))
-    im2 = imm2.resize((int(w2), int(h2)))
+def classify(unlab, lab, K = 1):
+    dist = [(L2(unlab, lab[0]), lab[1]) for lab in database]
+    dist = sorted(dist, key = lambda z: z[0])
+    dist = dist[ : K]
 
-    (w1, h1) = im1.size
-    (w2, h2) = im2.size
+    counts = { }
+    for z in dist:
+        if z[1] not in counts:
+            counts[z[1]] = 1
+        else:
+            counts[z[1]] += 1
 
-    (w, h) = (max(w1, w2), max(h1, h2))
-    region1 = (int(w / 2 - w1 / 2), int(h / 2 - h1 / 2))
-    region2 = (int(w / 2 - w2 / 2), int(h / 2 - h2 / 2))
+    labels = list(counts.items())
+    labels = sorted(labels, key = lambda z : z[1])
+    labels = list(reversed(labels))
 
-    new_im1 = Image.new('L', (w, h))
-    new_im2 = Image.new('L', (w, h))
+    return labels[0][0]
 
-    new_im1.paste(im1, region1)
-    new_im2.paste(im2, region2)
+# img1 = normalize(cv2.imread('database/apple-1.pgm', 0))
+# img2 = normalize(cv2.imread('database/apple-2.pgm', 0))
+# print(L2(*rescale(img1, img2)))
+# exit(0)
 
-    return (new_im1, new_im2)
+PATH = 'database/'
+LIMIT = 120
+files = [f for f in listdir(PATH) if isfile(join(PATH, f)) and '.pgm' in f]
+#files = files[ : LIMIT]
 
-im1 = Image.open('database/brick-1.pgm')
-(im1, s1) = change_basis(im1)
+database = []
+test = []
 
-im2 = Image.open('database/bat-7.pgm')
-(im2, s2) = change_basis(im2)
 
-(im1, im2) = mutual_rescale(im1, s1, im2, s2)
+#########################
+print('Reading files...')
+for f in files:
+    tokens = f.split('-')
+    class_name = tokens[0]
+    nb = int(tokens[1].split('.')[0])
 
-im1.show()
-im2.show()
+    features = normalize(cv2.imread(PATH + f, 0))
+
+    if nb % 2 == 0:
+        print('Adding {} to database'.format(f))
+        database.append([features, class_name])
+    else:
+        print('Adding {} to test'.format(f))
+        test.append([features, f, class_name])
+
+
+##############################
+print('Rescaling database...')
+scale_x = max(map(lambda lab : lab[0][1][0], database))
+scale_y = max(map(lambda lab : lab[0][1][1], database))
+
+for lab in database:
+    lab[0] = cv2.resize(lab[0][0], None, fx = scale_x / lab[0][1][0], fy = scale_y / lab[0][1][1])
+
+
+#######################
+print('Classifying...')
+
+count = 0
+db_count = { }
+
+for t in test:
+    if t[2] not in db_count:
+        db_count[t[2]] = [0, 1]
+    else:
+        db_count[t[2]][1] += 1
+
+    img = cv2.resize(t[0][0], None, fx = scale_x / t[0][1][0], fy = scale_y / t[0][1][1])
+    label = classify(img, database)
+    print('{} : {}'.format(t[1], label))
+
+    if label == t[2]:
+        count += 1
+        db_count[t[2]][0] += 1
+
+
+print('Global rate: {}'.format(count / len(test)))
+for k, v in db_count.items():
+    print('`{}` rate: {}/{}'.format(k, v[0], v[1]))
